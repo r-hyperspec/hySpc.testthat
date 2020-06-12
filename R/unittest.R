dbda935e507f727f3494073ea3ddf620f4bab5c8
Claudia Beleites <Claudia.Beleites@chemometrix.gmbh>
2020-06-12 23:18:20 +0200

    .Rbuildignore more RStudio files

--- R/unittest.R
+++ R/unittest.R
@@ -4,6 +4,8 @@
 ##' display the results.
 ##'
 ##' @rdname unittests
+##' @param ns namespace (package) to test
+##' @param standalone if `TRUE`, the appropriate [testthat::]
 ##' @return Invisibly returns a data.frame with the test results
 ##'
 ##' @author Claudia Beleites
@@ -15,42 +17,99 @@
 ##' @importFrom  testthat with_reporter
 ##' @importFrom  testthat get_reporter
 ##' @export
-hySpc.testthat.unittest <- function (){
-  if (!requireNamespace("testthat", quietly=TRUE)) {
+unittest <- function(ns, standalone = TRUE){
+  if (!requireNamespace("testthat", quietly = TRUE)) {
     warning("Package testthat required to run the unit tests.")
     return(NA)
   }
-  if (! "package:testthat" %in% search ())
+  if (!"package:testthat" %in% search())
     attachNamespace("testthat")
 
-  tests <- eapply(env = getNamespace ("hySpc.testthat"), FUN = get.test, all.names=TRUE)
-  tests <- tests [! sapply (tests, is.null)]
+  tests <- eapply(env = getNamespace(ns), FUN = get.test, all.names = TRUE)
+  tests <- tests[!sapply(tests, is.null)]
 
-  reporter <- SummaryReporter$new()
-  lister <- ListReporter$new()
-  reporter <- MultiReporter$new(reporters = list(reporter, lister))
-
-  with_reporter(reporter = reporter, start_end_reporter = TRUE, {
-    for (t in seq_along(tests)){
-      lister$start_file(names (tests [t]))
-      tests [[t]] ()
+  if (standalone) {
+    reporter <- ProgressReporter$new()
+    lister <- ListReporter$new()
+    reporter <- MultiReporter$new(reporters = list(reporter, lister))
+  
+    with_reporter(reporter = reporter, start_end_reporter = TRUE, {
+      for (t in seq_along(tests)) {
+        lister$start_file(names(tests[t]))
+        tests[[t]]()
+      }
+      get_reporter()$.end_context()
+    })
+    
+    invisible(lister$get_results())
+  } else {
+    for (t in seq_along(tests)) {
+      tests[[t]]()
     }
-    get_reporter()$.end_context()
-  })
-
-  invisible(lister$get_results())
-}
-
-##' @noRd
-{
-  `.test<-` <- function (f, value) {
-    attr (f, "test") <- value
-    f
   }
 }
 
+##' Attach unit tests to objects
+##'
+##' This function attaches unit tests in `value` to an object (typically a
+##' function) as an attribute `"test"`. 
+##'
+##' @param f the function (object) to which the tests are to be attached
+##' @param value the test code, a function with no parameters
+##' @return `f` with the test attached as attribute `"test"`
+##' @rdname unittests
+##' @export
+##' @examples 
+##' f <- function(x) x^2
+##' 
+##' .test(f) <- function() {
+##'    context("f")
+##'    
+##'    test_that("correct result for complex number", {
+##'      expect_equal(f(1i), -1 + 0i)
+##'    })
+##' }
+##' 
+`.test<-` <- function(f, value) {
+  attr(f, "test") <- value
+  f
+}
+
+.test(`.test<-`) <- function(){
+  context(".test<-")
+  
+  f <- function(x) x^2
+  test <- function() {"test"}
+
+  .test(f) <- test
+  
+  expect_equal(attr(f, "test"), test)
+}
+
 ##' get test that is attached to object as "test" attribute
-##' @noRd
-get.test <- function (object)
-  attr (object, "test")
+##'
+##' @rdname unittests
+##' @param object to which the test is attached
+##' @return the test (function)
+##' @export
+##' @examples 
+##' 
+##' get.test(f)
+##' 
+##' ## execute the test
+##' get.test(f)()
+##'
+get.test <- function(object){
+  attr(object, "test")
+}
+
+.test(`get.test`) <- function(){
+  context("get.test")
+  
+  f <- function(x) x^2
+  test <- function() {"test"}
+  .test(f) <- test
+  
+  expect_equal(get.test(f), test)
+}
 
