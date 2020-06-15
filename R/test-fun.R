9055923c208693b1307eb72ff2f862609b1dfd3e
Claudia Beleites <Claudia.Beleites@chemometrix.gmbh>
2020-06-15 14:07:55 +0200

    function to test single function

---
+++ R/test-fun.R
@@ -0,0 +1,48 @@
+##' Run test attached to function
+##'
+##' Execute test attached to a function with [testthat::Reporter].
+##'
+##' @param reporter [testthat::Reporter] to use
+##' @param object to which the test is attached
+##'
+##' @return the test (function)
+##' @include test.R
+##' @export
+##' @examples
+##'
+##' f <- function(x) x^2
+##' 
+##' .test(f) <- function() {
+##'    context("f")
+##'    
+##'    test_that("correct result for complex number", {
+##'      expect_equal(f(1i), -1 + 0i)
+##'    })
+##' }
+##'
+##' test_fun(f)
+##' 
+test_fun <- function(object, reporter = "minimal"){
+  test <- get.test(object)
+  with_reporter(reporter = reporter, test())
+}
+
+.test(`test_fun`) <- function(){
+  context("test_fun")
+  
+  test_that("correct execution", {
+    f <- function(x) x^2
+    
+    .test(f) <- function() {
+      context("f")
+      test_that("correct result for complex number", {
+        expect_equal(f(1i), -1 + 0i)
+      })
+    }
+    
+    res <- test_fun(f, reporter = "list")$get_results()
+    
+    expect_equal(res[[1]]$context, "f")
+    expect_true("expectation_success" %in% class(res[[1]]$results[[1]]))
+  })
+}
