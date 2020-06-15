f932b5a2473f47c5c499d93bf9e1ba2e05a677cd
Claudia Beleites <Claudia.Beleites@chemometrix.gmbh>
2020-06-15 14:07:41 +0200

    split files into one source file per function

---
+++ R/gettest.R
@@ -0,0 +1,45 @@
+##' Get test that is attached to object as "test" attribute
+##'
+##' The returned function can be executed. If that is done within a
+##' [testthat::Reporter], the results will be reported accordingly. Without
+##' reporter, the test runs silently if successful and stops if it fails.
+##'
+##' @param object to which the test is attached
+##' @return the test (function)
+##' @include test.R
+##' @export
+##' @examples
+##'
+##' f <- function(x) x^2
+##' 
+##' .test(f) <- function() {
+##'    context("f")
+##'    
+##'    test_that("correct result for complex number", {
+##'      expect_equal(f(1i), -1 + 0i)
+##'    })
+##' }
+##' 
+##' get.test(f)
+##'
+##' library(testthat)
+##' ## execute the test
+##' get.test(f)()
+##'
+##' with_reporter("summary", get.test(f)())
+##' 
+get.test <- function(object){
+  attr(object, "test")
+}
+
+.test(`get.test`) <- function(){
+  context("get.test")
+  
+  test_that("correct attribute is returned", {
+    f <- function(x) x^2
+    test <- function() {"test"}
+    .test(f) <- test
+    
+    expect_equal(get.test(f), test)
+  })
+}
---
+++ R/test.R
@@ -0,0 +1,38 @@
+##' Attach unit tests to objects
+##'
+##' This function attaches unit tests in `value` to an object (typically a
+##' function) as an attribute `"test"`. 
+##'
+##' @param f the function (object) to which the tests are to be attached
+##' @param value the test code, a function with no parameters
+##' @return `f` with the test attached as attribute `"test"`
+##' @export
+##' @examples 
+##' f <- function(x) x^2
+##' 
+##' .test(f) <- function() {
+##'    context("f")
+##'    
+##'    test_that("correct result for complex number", {
+##'      expect_equal(f(1i), -1 + 0i)
+##'    })
+##' }
+##' 
+`.test<-` <- function(f, value) {
+  attr(f, "test") <- value
+  f
+}
+
+.test(`.test<-`) <- function(){
+  context(".test<-")
+  
+  test_that("correct attribute is set", {
+    f <- function(x) x^2
+    test <- function() {"test"}
+    
+    .test(f) <- test
+    
+    expect_equal(attr(f, "test"), test)
+  })
+}
+
